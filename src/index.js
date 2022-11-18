3396ce5c668eb1ec8103a0a392bfcf4fd9fb9b83
lufor <1056252297@163.com>
2022-11-18 14:06:08 +0800

    add a div

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const targetURL = process.env.TARGET_URL
 class GetInfo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '', person: 'here is your result', QRnumber: 0, tickets: [1, 2, 3], code: '', shouleGO: Object };
+    this.state = { value: '', person: 'here is your result', QRnumber: 0, tickets: [1, 2, 3], code: '', shouleGO: Object, targeturl: '' };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.refreshPush = this.refreshPush.bind(this);
@@ -21,25 +21,25 @@ class GetInfo extends React.Component {
   }
   handleSubmit() {
     let regex = /^[0-9]$/
-    if (this.state.value.length == 11 && regex.test(this.state.value)==true) {
+    if (this.state.value.length == 11 && regex.test(this.state.value) == true) {
       // eslint-disable-next-line no-use-before-define
       let targetURL = targetURL + this.state.value
-      console.log(targetURL)
+      this.setState({ targeturl: targetURL })
       fetch(targetURL)
         .then(data => { return data.json() })
         .then(datum => {
-          if (datum.active != []&&datum.name!=undefined) {
+          if (datum.active != [] && datum.name != undefined) {
             console.log(datum.active);
             const result = JSON.stringify(datum, ['name', 'sex', 'age'], '');
             const shoulePost = datum.active.map((str) => <QRCodeSVG key={str} value={str} />);
             this.setState({ person: result, tickets: datum.active, QRnumber: datum.active.length, shouleGO: shoulePost });
           }
-         if(datum.name!=undefined && datum.active==[]) {
+          if (datum.name != undefined && datum.active == []) {
             const result = JSON.stringify(datum, ['name', 'sex', 'age'], '');
-            this.setState({person:result})
+            this.setState({ person: result })
           }
-          else{
-            this.setState({person:'plz check your number'})
+          else {
+            this.setState({ person: 'plz check your number' })
           }
         }
         );
@@ -73,6 +73,9 @@ class GetInfo extends React.Component {
         </button>
         <div>
           {this.state.person}
+        </div>
+        <div>
+          {this.state.targeturl}
         </div>
         <ul>{this.state.shouleGO}</ul>
       </div>
