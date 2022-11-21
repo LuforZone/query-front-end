9f98be2801d6cf2adbd317ac67aabe4462f431ab
lufor <1056252297@163.com>
2022-11-22 00:27:51 +0800

    console name

--- src/index.js
+++ src/index.js
@@ -40,7 +40,8 @@ class GetInfo extends React.Component {
             const result = JSON.stringify(datum, ['name', 'sex', 'age'], '');
             console.log(result);
             const shoulePost = datum.active.map((str) => <QRCodeSVG key={str} value={str} />);
-            this.setState({ person: "1111", tickets: datum.active, QRnumber: datum.active.length, shouleGO: shoulePost });
+            this.setState({ shouleGO: shoulePost, person: result });
+            //this.setState({ person: "1111", tickets: datum.active, QRnumber: datum.active.length, shouleGO: shoulePost });
             console.log(this.state.person);
           }
           if (datum.name != undefined && datum.active == []) {
